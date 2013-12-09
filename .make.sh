f04f165a587dd86e4bdfeadbe1cfcc903c811c28
makerdrone <makerdrone@gmail.com>
2013-12-09 10:51:59 +0200

    Updated .make.sh to copy over font folders. Added script to update font configuration.

--- .make.sh
+++ .make.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim tmux.conf gitconfig"    # list of files/folders to symlink in homedir
+files="vimrc vim tmux.conf gitconfig fonts fonts.conf.d"    # list of files/folders to symlink in homedir
 
 ##########
 

bd3578a6cc71cd3ff758d046f3bc78f0e8d97972
makerdrone <makerdrone@gmail.com>
2014-01-27 10:24:50 +0200

    Added call to set-mercurial-config.sh in make.sh

--- make.sh
+++ make.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim hgrc tmux.conf gitconfig"    # list of files/folders to symlink in homedir
+files="vimrc vim tmux.conf gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -29,3 +29,8 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+# set hgrc configuration
+echo "Creating mercurial configuration"
+set-mercurial-config.sh
+echo "...done"
